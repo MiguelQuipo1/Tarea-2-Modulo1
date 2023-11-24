18929134492fe568276b9847315080cb68375844
USUARIO <quipomiguel42@gmail.com>
2023-11-23 20:32:24 -0500

    V 0001 "Tarea 2 Modulo 1 Ejercicio Libreria"

---
+++ src/com/distribuida/entities/Categoria.java
@@ -0,0 +1,37 @@
+package com.distribuida.entities;
+
+public class Categoria {
+
+	private int idCategoria;
+	private String Categoria;
+	private String Descripcion;
+	
+	public Categoria() {}
+	
+	
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getCategoria() {
+		return Categoria;
+	}
+	public void setCategoria(String categoria) {
+		Categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [idCategoria=" + idCategoria + ", Categoria=" + Categoria + "]";
+	}
+	
+	
+}
